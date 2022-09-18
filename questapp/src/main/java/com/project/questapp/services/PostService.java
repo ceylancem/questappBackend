25d04c53a15e9f0136e48982f3d3a81dbd4ef10b
cemceylan <cemceylan@cem-macbook-pro>
2022-09-18 14:06:55 +0300

    questapp Application Backend

---
+++ questapp/src/main/java/com/project/questapp/services/PostService.java
@@ -0,0 +1,81 @@
+package com.project.questapp.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import com.project.questapp.entities.Post;
+import com.project.questapp.entities.User;
+import com.project.questapp.repos.PostRepository;
+import com.project.questapp.requests.PostCreateRequest;
+import com.project.questapp.requests.PostUpdateRequest;
+import com.project.questapp.responses.LikeResponse;
+import com.project.questapp.responses.PostResponse;
+
+@Service
+public class PostService {
+
+	private PostRepository postRepository;
+	private LikeService likeService;
+	private UserService userService;
+
+	public PostService(PostRepository postRepository, UserService userService, @Lazy LikeService likeService) {
+		this.postRepository = postRepository;
+		this.userService = userService;
+		this.likeService = likeService;
+	}
+
+//	public void setLikeService(LikeService likeService) {
+//		this.likeService = likeService;
+//	}
+
+	public List<PostResponse> getAllPosts(Optional<Long> userId) {
+		List<Post> list;
+		if (userId.isPresent()) {
+			list = postRepository.findByUserId(userId.get());
+		} else
+			list = postRepository.findAll();
+
+		return list.stream().map(p -> {
+			List<LikeResponse> likes = likeService.getAllLikesWithParam(Optional.ofNullable(null),
+					Optional.of(p.getId()));
+			return new PostResponse(p, likes);
+		}).collect(Collectors.toList());
+	}
+
+	public Post getOnePostById(Long postId) {
+		return postRepository.findById(postId).orElse(null);
+	}
+
+	public Post createOnePost(PostCreateRequest newPostRequest) {
+		User user = userService.getOneUserById(newPostRequest.getUserId());
+		if (user == null)
+			return null;
+		Post postToSave = new Post();
+		postToSave.setId(newPostRequest.getId());
+		postToSave.setText(newPostRequest.getText());
+		postToSave.setTitle(newPostRequest.getTitle());
+		postToSave.setUser(user);
+		return postRepository.save(postToSave);
+	}
+
+	public Post updateOnePostById(Long postId, PostUpdateRequest updatePost) {
+		Optional<Post> post = postRepository.findById(postId);
+		if (post.isPresent()) {
+			Post postToUpdate = post.get();
+			postToUpdate.setText(updatePost.getText());
+			postToUpdate.setTitle(updatePost.getTitle());
+			postRepository.save(postToUpdate);
+			return postToUpdate;
+		}
+		return null;
+	}
+
+	public void deleteOnePostById(Long postId) {
+		postRepository.deleteById(postId);
+	}
+
+}
